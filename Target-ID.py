a3b5daa7fd30053eef888137e398d16be69ab797
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-02-12 22:35:26 +0600

    Update Target-ID.py

--- Target-ID.py
+++ Target-ID.py
@@ -7,18 +7,7 @@ os.system('clear')
 print('\033[1;34m[•] Checking Updates...')
 os.system('git pull')
 bit = platform.architecture()[0]
-if bit=='64bit':
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
-bitt=platform.architecture()[0]
 
-if bitt=="64bit":
-    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
-    time.sleep(2)
-    import MX.muhid
-
-else:
-
-    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
 
 #---------------------------------------------------------#
 #                    THIS TOOL OWNED BY
