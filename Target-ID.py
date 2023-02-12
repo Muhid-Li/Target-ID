d4985416519efebd3bd226edc02113efbd1653ec
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-02-12 22:37:38 +0600

    Update Target-ID.py

--- Target-ID.py
+++ Target-ID.py
@@ -1,13 +1,3 @@
-#<\>!python3.11
-#-------------------Dont Edit This Script-----------------#
-import os,platform,time
-
-import os,platform
-os.system('clear')
-print('\033[1;34m[•] Checking Updates...')
-os.system('git pull')
-bit = platform.architecture()[0]
-
 
 #---------------------------------------------------------#
 #                    THIS TOOL OWNED BY
