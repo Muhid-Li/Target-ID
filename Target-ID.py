b0f9512922ef131cecc3517be08fb44e73d9825d
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-02-12 21:44:01 +0600

    Update Target-ID.py

--- Target-ID.py
+++ Target-ID.py
@@ -2,6 +2,13 @@
 #-------------------Dont Edit This Script-----------------#
 import os,platform,time
 
+import os,platform
+os.system('clear')
+print('\033[1;34m[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
 bitt=platform.architecture()[0]
 
 if bitt=="64bit":
