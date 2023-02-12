421160a3aebc2ee20515349eb9f3e780a7f039e6
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-02-12 21:34:51 +0600

    Update Target-ID.py

--- Target-ID.py
+++ Target-ID.py
@@ -1,8 +1,18 @@
-import os,platform
-os.system('clear')
-print('\033[1;34m[•] Checking Updates...')
-os.system('git pull')
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
-    os.system('xdg-open https://www.facebook.com/MUHID.LI.REPORT.DIYA.LAB.NAI7')
-    import FU3K3
-    FU3K3.menu()
+#<\>!python3.11
+#-------------------Dont Edit This Script-----------------#
+import os,platform,time
+
+bitt=platform.architecture()[0]
+
+if bitt=="64bit":
+    os.system('clear');print('[!] Your Device is 64 bit');time.sleep(1);print('\n\n[!] Your Python Version :');time.sleep(1);os.system('python --version')
+    time.sleep(2)
+    import MX.muhid
+
+else:
+
+    print('\nYOUR DEVICE 32 BIT NOT SUPPORT')
+
+#---------------------------------------------------------#
+#                    THIS TOOL OWNED BY
+#                  • MD. ABDUL MUHID •
