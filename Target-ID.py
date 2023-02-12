2abe153e3fc767b42ef9ff8c94e74c3a55f5f62a
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-02-12 21:29:36 +0600

    Update Target-ID.py

--- Target-ID.py
+++ Target-ID.py
@@ -2,8 +2,6 @@ import os,platform
 os.system('clear')
 print('\033[1;34m[•] Checking Updates...')
 os.system('git pull')
-bit = platform.architecture()[0]
-if bit=='32bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://www.facebook.com/MUHID.LI.REPORT.DIYA.LAB.NAI7')
     import FU3K3
