440dc8ae144c18a7413b19367ee66529ca2d7d4f
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-02-13 18:48:27 +0600

    Update Target-ID.py

--- Target-ID.py
+++ Target-ID.py
@@ -1,8 +1,3 @@
 import os,platform
 os.system('clear')
 print('\033[1;34m[•] Checking Updates...')
-os.system('git pull')
-
-    os.system('xdg-open https://www.facebook.com/I.am.MUHID.REPORT.DIYA.LAV.NAI')
-    import FU3K3
-    FU3K3.menu()
