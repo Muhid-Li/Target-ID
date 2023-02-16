e6f802abcbd60bd6604d9f61bff63df30f071689
Abdul-Muhid <61038788+Muhid-Li@users.noreply.github.com>
2023-02-16 16:17:54 +0600

    Update Target_ID.py

--- Target_ID.py
+++ Target_ID.py
@@ -1,23 +1,6 @@
-import platform
-import os
-try:os.system("xdg-open https://www.facebook.com/I.am.MUHID.REPORT.DIYA.LAV.NAI")
-except:pass	
-try:os.system("clear")
-except:pass	
-os.system('termux-setup-storage')
+import os,platform
+os.system('clear')
+print('\033[1;34m[•] Checking Updates...')
 os.system('git pull')
-try:os.system('touch /sdcard/james.txt')
-except:pass
-try:os.system('touch .proxy.txt')
-except:pass
-try:os.mkdir('/sdcard/OK')
-except:pass
-try:os.mkdir('/sdcard/CP')
-except:pass
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("latter")._site_view_()
-elif 'aarch' in arc:
-	__import__("Compact").ninex()
-else:
-	exit(f' Unknow device machine {arc}')
+
+print('\033[1;32m[•] Congrats! Your Device Support This Tools')
